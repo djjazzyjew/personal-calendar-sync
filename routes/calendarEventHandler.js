5dce4b9e3f126c041a4ab90ad8fcfac12c251fe8
djjazzyjew <baumana@gmail.com>
2022-10-04 14:18:54 -0600

    Added logic to create event

--- routes/calendarEventHandler.js
+++ routes/calendarEventHandler.js
@@ -11,6 +11,40 @@ var nylasAppConfigs = {
 // setup the Nylas API
 global.Nylas = require('nylas').config(nylasAppConfigs);
 
+function addEvent(calendarID) {
+    //const Nylas = require('nylas');
+    //Nylas.config({clientId: 'clientId', clientSecret: 'clientSecret'});
+    const nylas = Nylas.with(dest_access_token);
+
+    //Create a new event
+    const event = new Event(nylas);
+
+    // Provide the appropriate id for a calendar to add the event to a specific calendar
+    event.calendarId=calendarID;
+
+    // Start filling out other fields
+    event.title = '🎉 Party! 🎉';
+    event.location = 'My House!';
+    event.description = 'Let\'s celebrate our calendar integration!!';
+    event.busy = true;
+
+    // Participants are added as a list of EventParticipant objects
+    // email is required, name is optional
+    //event.participants = [new EventParticipant({name: 'My Nylas Friend', email: 'swag@nylas.com'})];
+   
+    // Set start time for tomorrow around current time
+    const startDate = new Date();
+    startDate.setMinutes((parseInt((startDate.getMinutes() + 37.5)/15) * 15) % 60);
+    startDate.setSeconds(0);
+
+    event.start = Date.parse(startDate)/1000 + (60*60*24); // Add 24 hours
+    event.end = event.start + (60*30); // Add 30 min
+
+    // .save()must be called to save the event to the third party provider
+    // The event object must have values assigned to calendarId and when before you can save it.
+    event.save({notify_participants: false});
+}
+
 router.get('/', function(req, res, next) {
     res.render("admin", { title: "Welcome to PCS event handler"});
 });
@@ -21,11 +55,13 @@ router.get('/test-calendar-conectivity', function(req, res, next) {
 
   // Load hardcoded calendars based on IDs from .env
   nylas_src.calendars.find(src_calendar_id).then(calendars => {
-    logger.debug(`Source Calendar = "${calendars.name}"`);
+    //logger.debug(`Source Calendar = "${calendars.name}"`);
   });
   
   nylas_dest.calendars.find(dest_calendar_id).then(calendars => {
-    logger.debug(`Destination Calendar = "${calendars.name}"`);
+    //logger.debug(`Destination Calendar = "${calendars.name}"`);
+
+    addEvent(dest_calendar_id);
   });
 
   res.render("admin", { title: "Testing Nylas connection" });
